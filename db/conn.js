ce52e852e20041a1cbeee44ee93d4181d0d01629
sambit985 <kumarsambit678@gmail.com>
2023-09-15 11:34:47 +0530

    Updated all files :rocket:

--- db/conn.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect('mongodb+srv://sambit98530:r7hxkr73skPEq2V5@cluster0.v1rtctx.mongodb.net/', { useNewUrlParser: true })
-    .then(() => {
-        console.log("Connection with db is Successfull");
-    }).catch((err) => {
-        console.log("Error while connection with db is", err);
-   })
--- ecommerce-userSignup/db/conn.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect('mongodb://127.0.0.1:27017/useDB', { useNewUrlParser: true })
-    .then(() => {
-        console.log("Connection with db is Successfull");
-    }).catch((err) => {
-        console.log("Error while connection with db is", err);
-   })
